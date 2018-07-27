71a6c30ce2c5929f49cf9f62f2a92a0758ab069d
puvvadamaniharish <41737685+puvvadamaniharish@users.noreply.github.com>
2018-07-27 22:57:58 +0530

    Update Character.java

--- Character.java
+++ Character.java
@@ -1,20 +1,25 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
-class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner h = new Scanner (System.in);
-		char  m = h.next.charAt();
-		   if (( m >= 'a' && m <= 'z') || (m >= 'A' && m <= 'Z'))
-       {
-          System.out.println("Alphabet");
-       }
-       else
-         System.out.println("No");
-       
+		int m = h.nextInt();
+		   if ( m > 0 )
+		    {
+		    	if ( m % 2 == 0 )
+		    	 {
+		    	 	System.out.println("Even");
+		    	 }
+		    	 else
+		    	 {
+		    	 	System.out.println("Odd");
+		    	 }
+		    }
+		    else
+		    {
+		    	System.out.println("invalid");
+		    }
 	}
 }
