2034d6e767bdc8ce7ee75d765573bdd6f0ea8cbd
puvvadamaniharish <41737685+puvvadamaniharish@users.noreply.github.com>
2018-07-27 22:46:29 +0530

    Create Alphabet.java

---
+++ Alphabet.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner h = new Scanner (System.in);
+		char  m = h.next.charAt();
+		   if (( m >= 'a' && m <= 'z') || (m >= 'A' && m <= 'Z'))
+       {
+          System.out.println("Alphabet");
+       }
+       else
+         System.out.println("No");
+       
+	}
+}
