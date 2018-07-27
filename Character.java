14af52a18231ac118f4edf6ec170e3be3cacf544
puvvadamaniharish <41737685+puvvadamaniharish@users.noreply.github.com>
2018-07-27 23:16:50 +0530

    Update Character.java

--- Character.java
+++ Character.java
@@ -1,25 +1,17 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
+class test
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		Scanner h = new Scanner (System.in);
-		int m = h.nextInt();
-		   if ( m > 0 )
-		    {
-		    	if ( m % 2 == 0 )
-		    	 {
-		    	 	System.out.println("Even");
-		    	 }
-		    	 else
-		    	 {
-		    	 	System.out.println("Odd");
-		    	 }
+		Scanner s = new Scanner (System.in);
+		char m = s.next().charAt(0);
+		 if ( (m >= 'a' && m  <= 'z') || (m >= 'A' && m <= 'Z' ) )
+		 {
+			 System.out.println("Alphabet");
 		    }
-		    else
-		    {
-		    	System.out.println("invalid");
-		    }
+		else
+			 System.out.println("No");
 	}
 }
